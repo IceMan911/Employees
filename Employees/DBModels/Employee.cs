b4aaa891198c4b65c58b1dd78c25ef69347d8ca4
David Novák <novak.david@cmsys.cz>
2022-12-08 22:37:04 +0100

    added class Employee

---
+++ Employees/DBModels/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employees.DBModels
+{
+    public class Employee
+    {
+        public uint id { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+
+        public DateTime dateOfBirth  { get; set; }
+    }
+}
