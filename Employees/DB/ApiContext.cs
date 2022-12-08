6c013255cc7d7535f95e038548c7ff5847e3139e
David Novák <novak.david@cmsys.cz>
2022-12-08 22:39:20 +0100

    added class ApiContext

---
+++ Employees/DB/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Employees.DBModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.DB
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> employees { get; set; }
+
+    }
+}
