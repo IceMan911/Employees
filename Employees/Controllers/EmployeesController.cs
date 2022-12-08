17c0d342cd447c2e95799c8633913b6ace362663
David Novák <novak.david@cmsys.cz>
2022-12-09 00:06:03 +0100

    added EmpleyeesController

---
+++ Employees/Controllers/EmployeesController.cs
@@ -0,0 +1,55 @@
+﻿using Employees.DB;
+using Employees.DBModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Employees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public EmployeesController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> getEmployee(uint id)
+        {
+
+            var employee = _context.employees.FirstOrDefault(x => x.id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
