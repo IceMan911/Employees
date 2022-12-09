5c6f93ef6e5d3f091689deac6cfb81343217e566
David Novák <novak.david@cmsys.cz>
2022-12-09 03:09:33 +0100

    added next data for testing

--- Employees/DB/DBData.cs
+++ Employees/DB/DBData.cs
@@ -17,7 +17,25 @@ namespace Employees.DB
                 dateOfBirth = new DateTime(1985, 11, 5).ToString("d.MM.yyyy")
             };
 
+            var testUser2 = new DBModels.Employee
+            {
+                id = 2,
+                name = "Adam",
+                surname = "Kopeček",
+                dateOfBirth = new DateTime(1995, 11, 5).ToString("d.MM.yyyy")
+            };
+
+            var testUser3 = new DBModels.Employee
+            {
+                id = 3,
+                name = "Jaromír",
+                surname = "Woronycz",
+                dateOfBirth = new DateTime(2000, 11, 5).ToString("d.MM.yyyy")
+            };
+
             context.employees.Add(testUser1);
+            context.employees.Add(testUser2);
+            context.employees.Add(testUser3);
 
             context.SaveChanges();
         }
